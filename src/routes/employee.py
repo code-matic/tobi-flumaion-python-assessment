258fff96c9b9043bde4029295d150d1c0c3e7a98
Oluwatobi Akintunlese <akintunlesetobi@gmail.com>
2025-03-11 14:24:15 +0100

    remove asyncs

--- src/routes/employee.py
+++ src/routes/employee.py
@@ -25,7 +25,7 @@ def get_retirement_service(
 
 
 @employee_router.get("/list-retiring-employees", response_model=RetirementResponse)
-async def get_retiring_employees(retirement_service: RetirementService = Depends(get_retirement_service)):
+def get_retiring_employees(retirement_service: RetirementService = Depends(get_retirement_service)):
     """
     Return a JSON response of employees retiring in the current year
     
@@ -38,7 +38,7 @@ async def get_retiring_employees(retirement_service: RetirementService = Depends
 
 
 @employee_router.post("/new-employee", response_model=CreateEmployeeResponse)
-async def add_employee(employee: BaseEmployee, employee_repository: AbstractRepository = Depends(get_employee_repository)):
+def add_employee(employee: BaseEmployee, employee_repository: AbstractRepository = Depends(get_employee_repository)):
     """
     Add a new employee to the repository
     """
