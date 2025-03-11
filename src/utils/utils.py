990bf6a3f36bd1811866acfe7c922511e58ce9d1
Oluwatobi Akintunlese <akintunlesetobi@gmail.com>
2025-03-11 14:14:09 +0100

    remove unused code

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,16 +1,6 @@
 
-import datetime
-from decimal import Decimal
-
 def api_response(data: dict, code: int = 0):
     return {
         "data": data,
         "code": code
     }
-
-def default_converter(o):
-    if isinstance(o, (datetime.date, datetime.datetime)):
-        return o.isoformat()  # convert date/datetime to ISO string
-    if isinstance(o, Decimal):
-        return float(o)       # convert Decimal to float
-    raise TypeError(f"Object of type {o.__class__.__name__} is not JSON serializable")
